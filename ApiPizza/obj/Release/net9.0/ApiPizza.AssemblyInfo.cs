f2913a0f00d833055dc055811584c3aa399c6eca
nThDepSuZz <jesusantana42@gmail.com>
2025-02-04 23:30:56 -0600

    Agregue mi Nombre

--- ApiPizza/obj/Release/net9.0/ApiPizza.AssemblyInfo.cs
+++ ApiPizza/obj/Release/net9.0/ApiPizza.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPizza")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e9884610a2d0f083ca513455ac9395a525222b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPizza")]
