5556bbde9851a73c6dc190952ef013f7b6252aa1
Jorge-CL16 <jorge.medi.08@gmail.com>
2025-02-04 22:48:01 -0600

    Agregue mi nombre

--- ApiPizza/Controllers/WeatherForecastController.cs
+++ ApiPizza/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ApiPizza.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Jorge"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
