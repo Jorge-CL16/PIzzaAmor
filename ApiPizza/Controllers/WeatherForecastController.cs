b57d34d996be2af9c5b3352c891c5f8b65a0982f
Jorge-CL16 <jorge.medi.08@gmail.com>
2025-02-05 23:32:49 -0600

    Deben de salir ahora si

--- ApiPizza/Controllers/WeatherForecastController.cs
+++ ApiPizza/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ApiPizza.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Jorge", "Jesus Santana","Juan Pablo"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Jorge Diaz", "Jesus Santana","Juan Pablo"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
